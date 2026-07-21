722d23ce019e3a40e9fb3c14eb38b27eb05f1348
responsecode502 <responsecode502@gmail.com>
2026-07-22 01:49:24 +0300

    ci-2026-07-22 01:49:24.143173

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Abyss Dotfiles
 
-**Dotfiles** is a symlink-based Dotfiles manager. 
+**A symlink-based Dotfiles manager. 
 
 ## Features
 * **JSON Output**: Output uses a strict JSON contract and is easy to parse.
--- ci/gen_readme.py
+++ ci/gen_readme.py
@@ -5,7 +5,7 @@ def generate_readme():
     
     content = """# Abyss Dotfiles
 
-**Dotfiles** is a symlink-based Dotfiles manager. 
+**A symlink-based Dotfiles manager. 
 
 ## Features
 * **JSON Output**: Output uses a strict JSON contract and is easy to parse.
